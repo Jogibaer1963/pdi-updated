378e64db45974e1fd851c450c08d137dec1c427d
Juergen Hauser <jogibaer99@gmail.com>
2018-01-30 01:41:36 -0600

    more improvements to check points

--- client/Collections/checklist.js
+++ client/Collections/checklist.js
@@ -10,13 +10,13 @@ if (Meteor.isClient) {
             const machineRangeEnd = event.target.machineRangeEnd.value;
             const resultStart = machineRangeStart.split(" ");
             const resultEnd = machineRangeEnd.split(" ");
-           const range = [];
-           $('input[name=range]:checked').each(function() {
+            const range = [];
+            $('input[name=range]:checked').each(function () {
                 range.push($(this).val());
             });
             const status = 1;
-           Meteor.call('inputNewCheckPoint', status, errorPos, errorDescription,
-                           range, resultStart, resultEnd);
+            Meteor.call('inputNewCheckPoint', status, errorPos, errorDescription,
+                range, resultStart, resultEnd);
             event.target.newPosition.value = "";
             event.target.errorDescription.value = "";
             event.target.C77.checked = false;
@@ -26,14 +26,14 @@ if (Meteor.isClient) {
             event.target.machineRangeEnd.value = "";
         },
 
-        'click .showCheckList': function() {
+        'click .showCheckList': function () {
             event.preventDefault();
             const checkPoint = this._id;
             console.log(checkPoint);
             Session.set('selectedCheckPoint', checkPoint);
         },
 
-        'click .deActiveCheck': function() {
+        'click .deActiveCheck': function () {
             event.preventDefault();
             const deactivateCheck = Session.get('selectedCheckPoint');
             console.log('deactivate', deactivateCheck);
@@ -46,71 +46,11 @@ if (Meteor.isClient) {
 
     Template.inputNewCheckPoint.helpers({
 
-        checkList: function () {
-            event.preventDefault();
-            Session.set('selectedCheckPoint', '');
-            return checkPoints.find({status: 1}, {sort: {errorPos: 1}});
-        },
 
-        inActiveCheckPoints: function() {
+
+        inActiveCheckPoints: function () {
             event.preventDefault();
             return checkPoints.find({status: 0}, {sort: {errorPos: 1}});
-        },
-
-        'selectedClass': function() {
-            const checkPoint = this._id;
-            const selectedCheckPoint = Session.get('selectedCheckPoint');
-            if (selectedCheckPoint === checkPoint) {
-                return "selected"
-            }
-        }
-
-
-
-
-    });
-/*
-
-    Template.input_new_checklistItems.events({
-
-        "submit .inputNewCheckListItems": function() {
-            event.preventDefault();
-            const errorPos = event.target.newPosition.value;
-            const errorDescription = event.target.errorDescription.value;
-            const machineType = event.target.machineType.value;
-            const machineArray = machineType.split(",");
-            const status = 1;
-            const checkId = Session.get('selectedCheckPoint');
-            Meteor.call('editCheckPoint', checkId, status, errorPos, errorDescription, machineArray);
-            event.target.newPosition.value = "";
-            event.target.errorDescription.value = "";
-            event.target.machineType.value = "";
-            },
-
-
-        'click .deActiveCheck': function() {
-            event.preventDefault();
-            const deactivateCheck = Session.get('selectedCheckPoint');
-            const status = 0;
-            Meteor.call('deactivateCheckPoint', deactivateCheck, status);
-        },
-
-        'click .showCheckList': function() {
-            event.preventDefault();
-            const checkPoint = this._id;
-            Session.set('selectedCheckPoint', checkPoint);
-            }
-
-
-    });
-  /*
-    Template.input_new_checklistItems.helpers({
-
-        checkList: function () {
-            event.preventDefault();
-            Session.set('selectedCheckPoint', '');
-            return checkPoints.find({status: 1}, {sort: {errorPos: 1}});
-
         },
 
         'selectedClass': function () {
@@ -119,41 +59,26 @@ if (Meteor.isClient) {
             if (selectedCheckPoint === checkPoint) {
                 return "selected"
             }
+        }
+
+
+    });
+
+    Template.checkPointTable.helpers({
+
+        checkList: function () {
+            event.preventDefault();
+            Session.set('selectedCheckPoint', '');
+            let loadedCheckPoints = checkPoints.find({status: 1}, {sort: {errorPos: 1}});
+            let countedCheckPoints = loadedCheckPoints.count();
+
+            console.log(countedCheckPoints);
+            return loadedCheckPoints;
         },
 
 
-        editCheckpoint: function () {
-            const editCheckPoint = Session.get('selectedCheckPoint');
-            return checkPoints.findOne({_id: editCheckPoint});
-        }
 
     });
-
-    
-    Template.deactivatedCheckPoints.helpers({
-
-      inActiveCheckPoints: function() {
-           event.preventDefault();
-           return checkPoints.find({status: 0}, {sort: {errorPos: 1}});
-       },
-
-        'selectedClass': function() {
-            const checkPoint = this._id;
-            const selectedCheckPoint = Session.get('selectedCheckPoint');
-            if (selectedCheckPoint === checkPoint) {
-                return "selected"
-            }
-        }
-    });
-
-    Template.deactivatedCheckPoints.events({
-
-        'click .showCheckList': function() {
-            event.preventDefault();
-            const checkPoint = this._id;
-            Session.set('selectedCheckPoint', checkPoint);
-        }
-    });
-     */
 
 }
+
