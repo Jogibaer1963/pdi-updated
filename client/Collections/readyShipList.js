5f17f56d6bc7ecd3d8357252190b5e9c39f7250b
Juergen Hauser <jogibaer99@gmail.com>
2018-03-03 00:00:28 -0600

    improved shipping status

--- client/Collections/readyShipList.js
+++ client/Collections/readyShipList.js
@@ -6,7 +6,7 @@ if (Meteor.isClient) {
             return MachineReady.find({shipStatus: 0, pdiStatus: 1, repairStatus: 1, washStatus: 1}, {sort: {date: 1}});
         },
 
-        'selectedClass': function() {
+        'selectedClass': function () {
             const checkPoint = this._id;
             const selectedCheckPoint = Session.get('selectedMachine');
             if (selectedCheckPoint === checkPoint) {
@@ -17,60 +17,29 @@ if (Meteor.isClient) {
 
     Template.overViewReadyList.events({
 
-        'click .readyList': function() {
+        'click .readyList': function () {
             const readyMachine = this._id;
-            Session.set('selectedMachine', readyMachine );
+            Session.set('selectedMachine', readyMachine);
         },
 
-        'click .shipMeButton': function() {
+        'click .shipMeButton': function () {
             event.preventDefault();
             const selectedCheckPoint = Session.get('selectedMachine');
             Meteor.call('machineIsGone', selectedCheckPoint);
         },
 
-        'submit .locationId': function(event) {
+        'submit .locationId': function (event) {
             event.preventDefault();
             const selectedPdiMachine = Session.get('selectedMachine');
-            if(typeof selectedPdiMachine === 'undefined') {
+            if (typeof selectedPdiMachine === 'undefined') {
                 alert('Mark the Machine first before update the Location');
             }
             const locationId = event.target.locationId.value;
             Meteor.call('locationUpdate', selectedPdiMachine, locationId);
-            event.target.locationId.value="";
+            event.target.locationId.value = "";
             Session.set('selectedMachine', '');
         }
     });
-
-
-
-    Template.shippingInProcess.helpers({
-        shippingOverView: function() {
-            return MachineReady.find({shipStatus: 2}, {sort: {date: 1}});
-        },
-
-        'selectedClass': function() {
-            const shippingMachine = this._id;
-            const selectedMachine = Session.get('selectedMachine');
-            if (shippingMachine == selectedMachine) {
-                return "selected"
-            }
-        }
-    });
-
-
-    Template.shippingInProcess.events({
-        'click .machineReadyShip': function () {
-            const shippingMachine = this._id;
-            Session.set('selectedMachine', shippingMachine);
-
-        },
-
-        'click .machineOnTrailer': function() {
-            const selectedCheckPoint = Session.get('selectedMachine');
-            Meteor.call('machineIsGone', selectedCheckPoint);
-        }
-    });
-
 
 
 }
