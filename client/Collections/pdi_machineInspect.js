e13cc418007ecaf8b54e99164f2370da671443eb
Juergen Hauser <jogibaer99@gmail.com>
2018-03-14 19:19:27 -0500

    Variant Viewer added

--- client/Collections/pdi_machineInspect.js
+++ client/Collections/pdi_machineInspect.js
@@ -19,59 +19,6 @@ if(Meteor.isClient) {
         },
 
 
-        ommMainBooklets: function () {
-            return ommMain.find({});
-        },
-
-        selectedMainOm: function() {
-            event.preventDefault();
-            return Session.get('selectedMainOm');
-        },
-        //-------------------------------------------------------------//
-        ommSupplement: function () {
-            return ommSupp.find({});
-        },
-
-        selectedSuppOm: function() {
-            event.preventDefault();
-            return Session.get('selectedSuppOm');
-        },
-        //------------------------------------------------------------//
-        ommUnload: function () {
-            return ommUnload.find({});
-        },
-
-        selectedUnloadOm: function() {
-            event.preventDefault();
-            return Session.get('selectedUnloadOm');
-        },
-        //--------------------------------------------------------------//
-
-        ommCebisMo: function () {
-            return ommCebisMo.find({});
-        },
-
-        selectedCemosOm: function() {
-            event.preventDefault();
-            return Session.get('selectedCemosOm');
-        },
-        //--------------------------------------------------------------//
-
-        ommTera: function () {
-            return ommTeraTrack.find({});
-        },
-
-        selectedTeraOm: function() {
-            event.preventDefault();
-            return Session.get('selectedTeraTrackOm');
-        },
-
-        //--------------------------------------------------------------//
-
-        ommProfi: function () {
-            return ommProfiCam.find({});
-        },
-
         selectedProfiCam: function() {
             event.preventDefault();
             return Session.get('selectedProfiCam');
@@ -116,54 +63,6 @@ if(Meteor.isClient) {
 
     Template.ommBook.events({
 
-        'click .omMain': function() {
-            event.preventDefault();
-            Session.set('selectedMainOm', '');
-            const omMainPart = this._id;
-            const selectedMainOm = ommMain.findOne({_id: omMainPart}).ommMain;
-            Session.set('selectedMainOm', selectedMainOm);
-        },
-
-        'click .omSupp': function() {
-            event.preventDefault();
-            Session.set('selectedSuppOm', '');
-            const omSupp = this._id;
-            const selectedSuppOm = ommSupp.findOne({_id: omSupp}).ommSupp;
-            Session.set('selectedSuppOm', selectedSuppOm);
-        },
-
-        'click .omUnload': function() {
-            event.preventDefault();
-            Session.set('selectedUnloadOm', '');
-            const omUnload = this._id;
-            const selectedUnloadOm = ommUnload.findOne({_id: omUnload}).omUnload;
-            Session.set('selectedUnloadOm', selectedUnloadOm);
-        },
-
-        'click .omCemos': function() {
-            event.preventDefault();
-            Session.set('selectedCemosOm', '');
-            const omCemos = this._id;
-            const selectedCemosOm = ommCebisMo.findOne({_id: omCemos}).ommCebisMobil;
-            Session.set('selectedCemosOm', selectedCemosOm);
-        },
-
-        'click .omTera': function() {
-            event.preventDefault();
-            Session.set('selectedTeraTrackOm', '');
-            const omTera = this._id;
-            const selectedTeraTrackOm = ommTeraTrack.findOne({_id: omTera}).ommTeraTrack;
-            Session.set('selectedTeraTrackOm', selectedTeraTrackOm);
-        },
-
-        'click .ommProfiCam': function() {
-            event.preventDefault();
-            Session.set('selectedProfiCam', '');
-            const omProfi = this._id;
-            Session.set('selectedProfiId', omProfi);
-            const selectedProfiCam = ommProfiCam.findOne({_id: omProfi}).ommProfiCam;
-            Session.set('selectedProfiCam', selectedProfiCam);
-        },
 
         'submit .omBooklets': function (event) {
             event.preventDefault();
