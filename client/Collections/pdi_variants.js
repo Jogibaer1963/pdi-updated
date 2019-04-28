286178cc2e39f30bfa62dd594460d152a35b2451
Juergen Hauser <jogibaer99@gmail.com>
2019-04-28 15:20:53 -0500

    excluding Variants now possible / bug fix

--- client/Collections/pdi_variants.js
+++ client/Collections/pdi_variants.js
@@ -21,17 +21,35 @@ if(Meteor.isClient) {
                    Session.set('variant-C77-count', variant_C77_length);
                    return variant_C77;
                case 2:
-                   return variants_C78.find({}, {sort: {variant: 1}});
+                  let variant_C78 = variants_C78.find({}, {sort: {variant: 1}});
+                   let variant_C78_length = variants_C78.find().count();
+                   Session.set('variant-C78-count', variant_C78_length);
+                   return variant_C78;
                case 3:
-                   return variants_C79.find({}, {sort: {variant: 1}});
+                   let variant_C79 = variants_C79.find({}, {sort: {variant: 1}});
+                   let variant_C79_length = variants_C79.find().count();
+                   Session.set('variant-C79-count', variant_C79_length);
+                   return variant_C79;
                case 4:
-                   return variants_C87.find({}, {sort: {variant: 1}});
+                   let variant_C87 = variants_C87.find({}, {sort: {variant: 1}});
+                   let variant_C87_length = variants_C87.find().count();
+                   Session.set('variant-C87-count', variant_C87_length);
+                   return variant_C87;
                case 5:
-                   return variants_C88.find({}, {sort: {variant: 1}});
+                  let variant_C88 = variants_C88.find({}, {sort: {variant: 1}});
+                   let variant_C88_length = variants_C88.find().count();
+                   Session.set('variant-C88-count', variant_C88_length);
+                   return variant_C88;
                case 6:
-                   return variants_C89.find({}, {sort: {variant: 1}});
+                   let variant_C89 = variants_C89.find({}, {sort: {variant: 1}});
+                   let variant_C89_length = variants_C89.find().count();
+                   Session.set('variant-C89-count', variant_C89_length);
+                   return variant_C89;
                case 7:
-                   return variants_C68.find({}, {sort: {variant: 1}});
+                   let variant_C68 = variants_C68.find({}, {sort: {variant: 1}});
+                   let variant_C68_length = variants_C68.find().count();
+                   Session.set('variant-C68-count', variant_C68_length);
+                   return variant_C68;
            }
         },
 
@@ -96,12 +114,28 @@ if(Meteor.isClient) {
         'click .submitViewer': () => {
           event.preventDefault();
           var variantChosen = Session.get('variantType');
+          if (variantChosen === 1) {
+              variantCount = Session.get('variant-C77-count')
+          } else if (variantChosen === 2) {
+              variantCount = Session.get('variant-C78-count')
+          } else if (variantChosen === 3) {
+              variantCount = Session.get('variant-C79-count')
+          } else if (variantChosen === 4) {
+              variantCount = Session.get('variant-C87-count')
+          } else if (variantChosen === 5) {
+              variantCount = Session.get('variant-C88-count')
+          } else if (variantChosen === 6) {
+              variantCount = Session.get('variant-C89-count')
+          } else if (variantChosen === 7) {
+              variantCount = Session.get('variant-C68-count')
+          }
           const variantVisible = [];
           $('input[name=visible]:checked').each(function() {
               variantVisible.push($(this).val());
           });
           Meteor.call('visibleVariants', variantChosen, variantVisible);
-          for (let i = 0; i < variantVisible.length; i++) {
+      //    var variantCount = Session.get('variant-C77-count');
+          for (let i = 0; i <= variantCount - 1; i++) {
               visible[i].checked= false;
           }
         },
