b5cf15bfd705572621c56d85e902950a3ba965c8
Juergen Hauser <jogibaer99@gmail.com>
2020-03-17 20:54:53 -0500

    update repair view

--- client/Collections/pdi_variants.js
+++ client/Collections/pdi_variants.js
@@ -134,20 +134,40 @@ Meteor.subscribe("variants_C87");
           }
         },
 
-            /*
-            'change .loadVariantPic': function(event) {
+
+            'click .submitPic': function(event) {
                 event.preventDefault();
                 console.log('inside');
-                var files = event.target.file;
+                const variantImage = [];
+                $('input[name=visible]:checked').each(function() {
+                    variantImage.push($(this).val());
+                });
+                console.log(variantImage);
+
+                /*
+                let files = event.target.file;
                 console.log(files);
-                for (var i = 0, ln = files.length; i < ln; i++) {
+                for (let i = 0, ln = files.length; i < ln; i++) {
                     Images.insert(files[i], function (err, fileObj) {
                         // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
                     });
                 }
-            }
 
-             */
+                 */
+            },
+
+        'change .loadVariantPic': (e) => {
+            e.preventDefault();
+            const file = e.target.files[0];
+            console.log(files);
+            for (let i = 0, ln = files.length; i < ln; i++) {
+                Images.insert(files[i], function (err, fileObj) {
+                    // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+                });
+            }
+        }
+
+
 
 
     });
